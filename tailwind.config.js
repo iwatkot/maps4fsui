9c442a964a700e0939bcf6d7e4a17e591e776fb6
iwatkot <iwatkot@gmail.com>
2025-08-15 16:22:53 +0200

    Theme issue fix.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import tailwindcss from "tailwindcss";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      // Your custom theme extensions
+    },
+  },
+  darkMode: 'media', // Use system preference
+};
