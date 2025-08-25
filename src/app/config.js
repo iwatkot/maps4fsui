97e31b7d37915764523beaa98517b6739fe117a4
iwatkot <iwatkot@gmail.com>
2025-08-25 14:48:18 +0200

    Update config.

--- src/app/config.js
+++ src/app/config.js
@@ -3,7 +3,7 @@ const publicHostName = process.env.NEXT_PUBLIC_HOSTNAME || '';
 
 const config = {
   // Access environment variables like this:
-  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL || 'maps4fs',
+  backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL || 'maps4fsapi:8000',
   publicHostName: publicHostName,
   isPublicVersion: publicHostName === 'maps4fs',
   bearerToken: process.env.NEXT_PUBLIC_BEARER_TOKEN || null,
