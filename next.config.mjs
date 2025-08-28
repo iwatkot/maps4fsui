52b17231882348d1c8291741268b54bda8156fcd
iwatkot <iwatkot@gmail.com>
2025-08-28 17:32:30 +0200

    Actions update.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  // Expose these server environment variables to the client at runtime
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.BACKEND_URL,
+    NEXT_PUBLIC_BEARER_TOKEN: process.env.BEARER_TOKEN,
+    NEXT_PUBLIC_HOSTNAME: process.env.HOSTNAME,
+  },
 };
 
 export default nextConfig;
